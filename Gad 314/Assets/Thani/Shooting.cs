21b4f39c1e4ebc4ea48380993f19905a3209c620
MTACreations <mohammad.almheiri@student.saedubai.com>
2025-10-23 11:30:25 +0400

    Shooting (Basic)

---
+++ Gad 314/Assets/Thani/Shooting.cs
@@ -0,0 +1,34 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float bSpeed;
+    public float firingRate, bDamage;
+
+    public Transform bTransform;
+    public GameObject bPrefab;
+
+    private float timer;
+
+    private void Update()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime / firingRate;
+        }
+
+        if(Input.GetButtonDown("Fire1") && timer <= 0)
+        {
+            Shoot();
+            Debug.Log("He shoots!");
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bPrefab, bTransform.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody>().AddForce(bTransform.forward * bSpeed, ForceMode.Impulse);
+        bullet.GetComponent<BulletBehavior>().pain = bDamage;
+    }
+}

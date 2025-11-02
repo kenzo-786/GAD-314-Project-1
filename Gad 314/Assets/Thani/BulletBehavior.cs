ccb674b43a95c2b72adac5cbe0229b5068c47995
MTACreations <mohammad.almheiri@student.saedubai.com>
2025-11-02 18:31:07 +0400

    Enemies now take damage

--- Gad 314/Assets/Thani/BulletBehavior.cs
+++ Gad 314/Assets/Thani/BulletBehavior.cs
@@ -4,7 +4,6 @@ public class BulletBehavior : MonoBehaviour
 {
     public float pain;
     public float secondsToLive = 1;
-    public EnemyHealth enemy;
 
     private void Update()
     {
@@ -21,17 +20,6 @@ public class BulletBehavior : MonoBehaviour
         if (other.gameObject.tag == "Terrain")
         {
             Debug.Log("Hit the level!");
-            Destroy(gameObject);
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Enemy")
-        {
-            enemy = other.gameObject.GetComponent<EnemyHealth>();
-            Debug.Log("Hit an enemy!");
-            pain -= enemy.health;
             Destroy(gameObject);
         }
     }
--- Gad 314/Assets/Thani/EnemyHealth.cs
+++ Gad 314/Assets/Thani/EnemyHealth.cs
@@ -2,13 +2,27 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public int health = 100;
+    public float health = 100;
+    public BulletBehavior bullet;
 
     void Update()
     {
         if (health <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            bullet = other.gameObject.GetComponent<BulletBehavior>();
+            Debug.Log("Enemy hit!");
+            health -= bullet.pain;
+            Debug.Log(health);
+            Destroy(other.gameObject);
+            Debug.Log("Bullet destroyed!");
         }
     }
 }

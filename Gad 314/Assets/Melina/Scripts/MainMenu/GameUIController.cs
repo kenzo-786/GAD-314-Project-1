035d8f529d52c8a697b4f74e7d99ca5a1906ec15
hoiitsbeast <hoiitsmel@gmail.com>
2025-11-27 05:33:11 +0400

    trying to fix script

--- Gad 314/Assets/Melina/Scripts/MainMenu/GameUIController.cs
+++ Gad 314/Assets/Melina/Scripts/MainMenu/GameUIController.cs
@@ -5,42 +5,49 @@ public class GameUIController : MonoBehaviour
 {
     public GameObject pausePanel;
     public GameObject controlsPanel;
+    public GameObject pauseButton;
 
     void Start()
     {
         pausePanel.SetActive(false);
         controlsPanel.SetActive(false);
+        pauseButton.SetActive(true);
     }
 
     public void TogglePause()
     {
-        if (pausePanel.activeSelf)
+        if (!pausePanel.activeSelf && !controlsPanel.activeSelf)
         {
-            pausePanel.SetActive(false);
-            Time.timeScale = 1f;
+            pausePanel.SetActive(true);
+            pauseButton.SetActive(false);
+            Time.timeScale = 0f;
         }
         else
         {
-            pausePanel.SetActive(true);
-            Time.timeScale = 0f;
+            pausePanel.SetActive(false);
+            controlsPanel.SetActive(false);
+            pauseButton.SetActive(true);
+            Time.timeScale = 1f;
         }
-    }
-
-    public void QuitToMainMenu()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(0);
     }
 
     public void OpenControls()
     {
         controlsPanel.SetActive(true);
         pausePanel.SetActive(false);
+        pauseButton.SetActive(false);
     }
 
     public void BackFromControls()
     {
         controlsPanel.SetActive(false);
         pausePanel.SetActive(true);
+        pauseButton.SetActive(false);
+    }
+
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
     }
 }
